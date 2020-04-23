f1cb202548820eee2b31381b9ef4e0dd91975391
LiangsLi <poales@sina.com>
2020-04-23 16:54:45 +0800

    Add shebang

--- distributed_train.sh
+++ distributed_train.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 python -m torch.distributed.launch --nproc_per_node=6 main.py -c config_files/bert_biaffine.yaml
--- train.sh
+++ train.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py train -c config_files/bert_biaffine.yaml --test_after_train
